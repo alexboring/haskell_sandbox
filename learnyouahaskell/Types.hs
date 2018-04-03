dc652a07a98cfdd2634c6aae0931d1116c0103c7
Alexander Boring <alexander.s.boring@gmail.com>
2018-04-03 18:47:26 -0400

    Add Readme

--- learnyouahaskell/Types.hs
+++ learnyouahaskell/Types.hs
@@ -22,20 +22,6 @@ type AssocList k v = [(k, v)]
 get :: AssocList a b -> a -> b
 get xs k = find (\(x, y)-> x == k) xs
 
--- Each node is either red or black.
---
--- The root is black. This rule is sometimes omitted. Since the root can always be
---  changed from red to black, but not necessarily vice versa, this rule has little
---  effect on analysis.
---
--- All leaves (NIL) are black.
---
--- If a node is red, then both its children are black.
---
--- Every path from a given node to any of its descendant NIL nodes contains the
--- same number of black nodes. Some definitions: the number of black nodes from the
---  root to a node is the node's black depth; the uniform number of black nodes in
---  all paths from root to the leaves is called the black-height of the red–black tree.[17]
 
 
 data Animal a = Cat a | Dog a deriving (Show)
